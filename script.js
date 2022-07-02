d58436f2aef6ec17bc1cac9d1cc659f4f9836368
achtac <89649965+achtac@users.noreply.github.com>
2022-07-02 21:56:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,22 @@ home.onclick = ()=> {
    homebar.style.display ="block";
 
 }
+
+close.onclick = ()=> {
+  const sr = ScrollReveal({
+    reset:true
+  });
+
+
+  sr.reveal("#homebar",{
+
+    duration:500,
+    Delay:2,
+    distance:"-40px",
+    origin:"top"
+
+  });
+
+   homebar.style.display ="none";
+
+}
